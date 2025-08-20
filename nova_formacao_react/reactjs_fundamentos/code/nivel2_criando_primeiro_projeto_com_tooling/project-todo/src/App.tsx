7eeddabf6adfd0b4e24c325f98b906c405c67935
BManduca <brunnomanducarfe@gmail.com>
2025-08-20 17:29:48 -0300

    feat: finalizado a criação/estruturação de todos os componentes que serão utilizados na aplicação | Introduzido a biblioteca cva para gerenciar variações de estilo, facilitando a personalização dos próprios componentes

--- nova_formacao_react/reactjs_fundamentos/code/nivel2_criando_primeiro_projeto_com_tooling/project-todo/src/App.tsx
+++ nova_formacao_react/reactjs_fundamentos/code/nivel2_criando_primeiro_projeto_com_tooling/project-todo/src/App.tsx
@@ -1,45 +1,81 @@
-import CheckIcon from './assets/icons/check_regular.svg?react'
+import CheckIcon from './assets/icons/check.svg?react'
 import PencilIcon from './assets/icons/pencil_simple_regular.svg?react'
 import PlusIcon from './assets/icons/plus_regular.svg?react'
 import SpinnerIcon from './assets/icons/spinner.svg?react'
 import TrashIcon from './assets/icons/trash.svg?react'
 import XIcon from './assets/icons/x_regular.svg?react'
 import Badge from './components/badge'
+import Button from './components/button'
+import ButtonIcon from './components/button-icon'
+import Card from './components/card'
+import Container from './components/container'
 
 import Icon from './components/icon'
+import InputCheckbox from './components/input-checkbox'
+import InputText from './components/input-text'
+import Skeleton from './components/skeleton'
 import Text from './components/text'
 
 export function App() {
   return (
-    <div className="grid gap-3">
-      <div className="flex flex-col gap-2">
-        <Text className="text-pink-base" variant={'body-sm-bold'}>
-          <h1>Olá Mundo</h1>
-        </Text>
-        <Text className="text-green-base">
-          <h1>Olá Mundo</h1>
-        </Text>
-        <Text variant={'body-md-bold'}>
-          <h1>Olá Mundo</h1>
-        </Text>
-        <Text>
-          <h1>Levar o dog para passear</h1>
-        </Text>
-      </div>
+    <Container>
+      <div className="grid gap-10">
+        <div className="flex flex-col gap-2">
+          <Text className="text-pink-base" variant={'body-sm-bold'}>
+            <h1>Olá Mundo</h1>
+          </Text>
+          <Text className="text-green-base">
+            <h1>Olá Mundo</h1>
+          </Text>
+          <Text variant={'body-md-bold'}>
+            <h1>Olá Mundo</h1>
+          </Text>
+          <Text>
+            <h1>Levar o dog para passear</h1>
+          </Text>
+        </div>
+        <div className="flex gap-1">
+          <Icon className="fill-pink-base" svg={TrashIcon} />
+          <Icon className="fill-green-base" svg={CheckIcon} />
+          <Icon className="fill-pink-dark" svg={PencilIcon} />
+          <Icon className="fill-green-dark" svg={PlusIcon} />
+          <Icon svg={XIcon} />
+          <Icon animate svg={SpinnerIcon} />
+        </div>
+        <div className="flex gap-1">
+          <Badge variant={'secondary'}>5</Badge>
+          <Badge variant={'primary'}>2 de 5</Badge>
+          <Badge loading>5</Badge>
+        </div>
+        <div>
+          <Button icon={PlusIcon}>Nova tarefa</Button>
+        </div>
+        <div className="flex gap-1">
+          <ButtonIcon icon={TrashIcon} />
+          <ButtonIcon icon={TrashIcon} variant={'secondary'} />
+          <ButtonIcon icon={TrashIcon} variant={'tertiary'} />
+          <ButtonIcon icon={TrashIcon} loading />
+        </div>
+        <div>
+          <InputText />
+        </div>
 
-      <div className="flex gap-1">
-        <Icon className="fill-pink-base" svg={TrashIcon} />
-        <Icon className="fill-green-base" svg={CheckIcon} />
-        <Icon className="fill-pink-dark" svg={PencilIcon} />
-        <Icon className="fill-green-dark" svg={PlusIcon} />
-        <Icon svg={XIcon} />
-        <Icon animate svg={SpinnerIcon} />
-      </div>
+        <div>
+          <InputCheckbox />
 
-      <div>
-        <Badge variant={'secondary'}>5</Badge>
-        <Badge variant={'primary'}>2 de 5</Badge>
+          <InputCheckbox loading />
+        </div>
+
+        <div>
+          <Card size={'md'}>Olá Mundo!</Card>
+        </div>
+
+        <div className="space-y-2">
+          <Skeleton className="h-8" />
+          <Skeleton className="h-8" />
+          <Skeleton className="h-8 w-96" />
+        </div>
       </div>
-    </div>
+    </Container>
   )
 }
