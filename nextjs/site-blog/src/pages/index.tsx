d30bcfabd03adb84bddfbe06975fadeaa655e477
Brunno Manduca do Prado <brunnomanducarfe@gmail.com>
2026-03-12 19:14:45 -0300

    feat: Iniciado implementação do projeto | Criado component Header

--- nextjs/site-blog/src/pages/index.tsx
+++ nextjs/site-blog/src/pages/index.tsx
@@ -1,23 +1,23 @@
-import { Header } from "@/components/header/header";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google"
+import { Header } from "@/components/header"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
 export default function Home() {
   return (
     /* 
-    `${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_if_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    {`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     */
-    <div className='flex flex-wrap items-center gap-2 md:flex-row p-4'>
+    <div className="min-h-screen min-w-full bg-slate-400">
       <Header />
     </div>
-  );
+  )
 }
