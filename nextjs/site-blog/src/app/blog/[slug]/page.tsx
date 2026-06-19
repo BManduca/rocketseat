5a4cec3b10ec56f0115994791272cfea50d410e3
Brunno Manduca do Prado <brunnomanducarfe@gmail.com>
2026-06-19 11:19:14 -0300

    feat(blog): implementa generateStaticParams e revalidação via ISR
    - Gera os parâmetros estáticos para as páginas de post dinamicamente
    - Adiciona configuração de revalidação de 60 segundos (ISR)

--- nextjs/site-blog/src/app/blog/[slug]/page.tsx
+++ nextjs/site-blog/src/app/blog/[slug]/page.tsx
@@ -8,7 +8,18 @@ type BlogPostPage = {
   }>
 }
 
+// revalida a cada 60 segundos, para que o next crie novamente
+// sem a necessidade de dar build da aplicação inteira novamente
+export const revalidate = 60
+
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    slug: post.slug,
+  }))
+}
+
 export default async function BlogPostPage({ params }: BlogPostPage) {
+  // server side
   const { slug } = await params
   const post = allPosts.find((post) => post.slug === slug)
 
