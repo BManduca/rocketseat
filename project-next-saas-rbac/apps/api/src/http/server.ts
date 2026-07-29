a37af85f8cfed0d7ebdf065a168710084802b143
Brunno Manduca do Prado <brunnomanducarfe@gmail.com>
2026-07-29 15:00:33 -0300

    feat(api): adiciona rotas para transferencia de organizacao e criacao de projeto

--- project-next-saas-rbac/apps/api/src/http/server.ts
+++ project-next-saas-rbac/apps/api/src/http/server.ts
@@ -24,7 +24,9 @@ import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
 import { getOrganizations } from './routes/orgs/get-organizations'
 import { shutdownOrganization } from './routes/orgs/shutdown-organization'
+import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
+import { createProject } from './routes/projects/create-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -83,6 +85,10 @@ app.register(getOrganization)
 app.register(getOrganizations)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
+app.register(transferOrganization)
+
+// 💼 Projects
+app.register(createProject)
 
 app
   .listen({
